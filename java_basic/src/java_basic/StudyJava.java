09c3efb5f71107c053d4bd3a296e4d678a8f92cd
Appinnn <dltkdals759@gmail.com>
2024-06-25 17:30:31 +0900

    java0625

--- java_basic/src/java_basic/StudyJava.java
+++ java_basic/src/java_basic/StudyJava.java
@@ -5,7 +5,7 @@ public class StudyJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("자바테스트1212");
+		System.out.println("자바테스트12121");
 
 	}
 
