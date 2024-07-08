071c11b293e25b5c95fb546a2757f296d8c5607e
Appinnn <dltkdals759@gmail.com>
2024-07-08 17:31:09 +0900

    0708

--- Study/src/study1/study2.java
+++ Study/src/study1/study2.java
@@ -9,24 +9,54 @@ public class study2 {
 		// 자바의 자료구조
 		// 자료구조란 데이터를 효율적으로 저장, 관리하기 위한 구조
 		
-		ArrayList<String> arr = new ArrayList<String>();
+//		ArrayList<String> arr = new ArrayList<String>();
+//		
+//		arr.add("이순신");
+//		arr.add("김경제");
+//		arr.add("송영진");
+//		arr.add("송미연");
+//
+//		System.out.println( arr );  //테스트용 출력 
+//		
+//		for(int i=0; i<arr.size(); i++)
+//		{
+//			System.out.println( arr.get(i) );
+//		}
+//		
+//		for(String name : arr)
+//		{
+//			System.out.println( name );
+//		}
 		
-		arr.add("이순신");
-		arr.add("김경제");
-		arr.add("송영진");
-		arr.add("송미연");
-
-		System.out.println( arr );  //테스트용 출력 
+		// ArrayList를 사용하기 - 어려우면 정적배열로 String[] 방식
+		// 이름 10개 저장
+		// 이름 중에서 김 씨만 출력하기
 		
-		for(int i=0; i<arr.size(); i++)
+		ArrayList<String> name = new ArrayList<String>();
+		name.add("이순신");
+		name.add("김이박");
+		name.add("박세종");
+		name.add("이명박");
+		name.add("허경영");
+		name.add("김유신");
+		name.add("이방원");
+		name.add("장영실");
+		name.add("김춘추");
+		name.add("이성계");
+		
+//		for(String save : name)
+//		{
+//			if(save.contains("김"))
+//			{
+//				System.out.println(save);
+//			}
+//		}
+		for(int i=0; i<name.size(); i++)
 		{
-			System.out.println( arr.get(i) );
+			if(name.contains("김"))
+				System.out.println(name);
 		}
-		
-		for(String name : arr)
-		{
-			System.out.println( name );
-		}
+	
 	}
 
 }
--- java_inherit/src/homeProduct/Tv.java
+++ java_inherit/src/homeProduct/Tv.java
@@ -2,26 +2,57 @@ package homeProduct;
 
 public class Tv extends House
 {
-	int channel = 10; // 채널
-	int vol = 5;     // 소리
+	private int channel = 10; // 채널
+	private int vol = 5;     // 소리
 	
-	Tv() // 생성자 메서드
+	
+	public int getChannel() {
+		return channel;
+	}
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	public int getVol() {
+		return vol;
+	}
+	public void setVol(int vol) {
+		this.vol = vol;
+	}
+	
+	
+	
+	public Tv() // 생성자 메서드
 	{
 		super();
 	}
-	Tv(String brand, int price) // 매개 변수가 있는 생성자 메서드
+	public Tv(String brand, int price) // 매개 변수가 있는 생성자 메서드
 	{
 		super(brand,price); // 부모클래스 생성자메서드 호출
 	}
 	
-	void channelUp()
+	public void channelUp()
 	{
 		this.channel++;
 	}
 	
-	void channelDown()
+	public void channelDown()
 	{
 		this.channel--;
 	}
 	
+	@Override
+	public void power()
+	{
+		super.onOff = !super.onOff;
+		System.out.println("Tv전원 : " + super.onOff);
+	}
+	
 }
+// 오버로딩은 - 새로운 메서드를 정의하는것
+// 오버라이딩은 - 기존의 메서드를 재정의 하는것
+
+
+// 오버라이딩 조건
+// 1.부모클래스에 해당 메서드가 존재해야한다.
+// 2.오버라이딩 메서드는 반드시 반환타입, 메서드 이름, 매개변수가 모두 같아야한다.
+// 3.메서드의{ } 내용은 달라도 된다.
